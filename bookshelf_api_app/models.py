413b1908c866accaba6f0161147d78c222c62186
Zakky <kycraft1164@gmail.com>
2022-08-03 13:18:28 +0700

    create method in model to return dict of data

--- bookshelf_api_app/models.py
+++ bookshelf_api_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(
+        max_length = 64, 
+        null = False, 
+        default = "Tidak Berjudul")
+    description = models.CharField(
+        max_length = 128, 
+        null = False, 
+        default = "Tidak Berjudul")
+
+    def as_dict(self):
+        ''' return data with dictionary format (serialize each object returned not all of the objects returned) '''
+
+        return dict(
+            id = self.id,
+            title = self.title,
+            description = self.description
+        )
