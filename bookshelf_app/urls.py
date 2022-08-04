4e6fe59434e5c79e1cc85ea90207ff200e3eb053
Zakky <kycraft1164@gmail.com>
2022-08-04 09:40:49 +0700

    change views.py to api_views.py

--- bookshelf_app/urls.py
+++ bookshelf_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from . import api_views
 
 app_name = 'bookshelf_app'
 
 urlpatterns = [
-    path('api/books', views.getAllBooks, name='get-all-books'),
-    path('api/book/<int:id>', views.getAllBooks, name='get-book'),
-    path('api/create', views.createBook, name='create-book'),
+    path('api/books', api_views.getAllBooks, name='get-all-books'),
+    path('api/book/<int:id>', api_views.getAllBooks, name='get-book'),
+    path('api/create', api_views.createBook, name='create-book'),
 ]
