b2d1e033bd4eee48ed0f325f3fc130e044eac89d
Zakky <kycraft1164@gmail.com>
2022-08-06 11:24:31 +0700

    remove serializers import

--- bookshelf_app/api_views.py
+++ bookshelf_app/api_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt #, csrf_protect, requires_csrf_token
 import json
 
